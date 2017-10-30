2bac42488bdb15301657770f838b17a2a1e1b243
alex182 <format255@bk.ru>
2017-10-30 16:34:33 +0300

    1 commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Main extends React.Component {
+   render() {
+      return (
+          <h1>Hello</h1>
+      )
+   }
+}
+
